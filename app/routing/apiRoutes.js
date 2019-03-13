f3780b1dc841f5fa447b8da5a26c862f32e277c4
laurenkerns <laurenkerns@gmail.com>
2019-03-13 15:38:23 -0400

    added matches to friends.js and JQ logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,19 +3,43 @@
 
 const possibleFriends = require("../data/friends.js");
 
+//export module
 module.exports = function(app){
+
 //GET REQUEST//handles when users visit a page
 
 app.get("/api/friends", function(req,res){
     return res.json(possibleFriends);
+
 });
 
 //POST REQUEST//server responds to request
 //Save data once submitted
 app.post("/api/friends", function(req,res){
-    let friendMatch = req.body;
-    possibleFriends.push(friendMatch);
-    res.json(findFriend(friends));
+    //Use data from survey and store in variables
+    let friendScore = req.body.score;
+    let newFriend = {
+        nfName: "",
+        nfPhoto: "",
+        nfDifference: 15
+    };
+
+
+    // loop through all friends 
+    for (var i = 0; i < possibleFriends.length; i++){
+        let totalDifference = 0;
+            for( var f = 0; i <possibleFriends[i].length; i++){
+                totalDifference += Math.abs(friendScore[f] - possibleFriends[i].scores[f]);
+            }
+            if (totalDifference <= newFriend.nfDifference){
+                newFriend.nfName = possibleFriends[i].name;
+                newFriend.nfPhoto = possibleFriends[i].photo;
+            }
+    }
+    //push to friends array
+    possibleFriends.push(req.body);
+    res.json(newFriend)
+
     
 });
 }
